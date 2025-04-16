ce33d7d8ed84b1076f96c2f7e4332be1bc167c9b
Curry50 <872740080@qq.com>
2025-04-16 15:28:12 +0800

    first

--- isaacgymenvs/tasks/magnetic_ur5.py
+++ isaacgymenvs/tasks/magnetic_ur5.py
@@ -287,7 +287,6 @@ class MagneticUr5(VecTask):
         self.magnet_pos = self.magnet_states[:,:,0:3].squeeze()
         self.to_target = self.target_pos - self.capsule_pos
 
-        # self.obs_buf = torch.cat((self.capsule_pos,self.magnet_pos,self.target_pos,self.ur5_dof_pos,self.to_target),dim=-1)
         self.obs_buf = torch.cat((self.capsule_pos,self.target_pos,
                                   self.magnet_pos),dim=-1)
 
@@ -588,25 +587,6 @@ class MagneticUr5(VecTask):
 
         plt.legend()
         plt.show()
-
-    def square_wave_path(self):
-        A = 0.01
-        T = 0.04
-        L = 2 * T
-        num_points = 10
-
-        t = torch.linspace(0,L,num_points,device=self.device)
-        y = t
-        z = A * torch.where(torch.sin(2 * torch.pi * t / T) >= 0, 0.0, -1.0)
-        x = torch.zeros_like(t)
-
-        if self.viewer and self.debug_viz:
-            self.gym.clear_lines(self.viewer)
-            for i in range(self.num_envs):
-                for j in range(num_points - 1):
-                    p_start = torch.tensor([x[j]+0.5,y[j]+0.1,z[j]+0.325],device=self.device).cpu().numpy()
-                    p_end = torch.tensor([x[j+1]+0.5,y[j+1]+0.1,z[j+1]+0.325],device=self.device).cpu().numpy()
-                    self.gym.add_lines(self.viewer,self.envs[i],1,[p_start,p_end],[0.0,0.0,1.0])
 
 @torch.jit.script
 def compute_ur5_reward(reset_buf,progress_buf,max_episode_length,to_target,
